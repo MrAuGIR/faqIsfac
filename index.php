457a9686b72790bf43f5fcb3e4b80c2325e00809
handgood <handgood86@gmail.com>
2021-02-16 11:54:01 +0100

    accordeon ok

--- index.php
+++ index.php
@@ -4,9 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="img/favicon.ico" rel="icon" type="image/png"/>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"/>
+    <link href="styles/styles.css" rel="stylesheet"/>
     <title>Document</title>
 </head>
 <body>
     <h1>Test</h1>
+    <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Accordion Item #1
+            </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA v v LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
