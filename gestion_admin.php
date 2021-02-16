452b77ff6a4d2059a112832f80e5f68982add498
Juju-D <juliettedeligny31@gmail.com>
2021-02-16 13:40:47 +0100

    modification

--- gestion_admin.php
+++ gestion_admin.php
@@ -5,31 +5,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="styles/style_gestion.css"/> 
     <title>Document</title>
 </head>
 <body>
     <main>
-        <table>
-            <tr>
-                <td>Question:</td>
-                <td>Modifier:</td>
-                <td>Supprimer:</td>        
-            </tr>
-            <?php foreach($faq as $question):?>
-                <tr>
-                    <td>
-                        <?= $question['question'] ?>
-                    </td> 
-                    <td>
-                        <a href="modifier.php?&id=$id" title="modifier"><i class="bi bi-pencil-square"></i></a>
-                    </td>
-                    <td>
-                        <a href="supprimer.php?&id=$id" title="supprimer"><i class="bi bi-x-square"></i></a>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
+        <div class="container">
+            <table class="table">
+                <thead class="table-light">
+                    <tr>
+                        <td>Question</td>
+                        <td>Modifier</td>
+                        <td>Supprimer</td>        
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <?php foreach($faq as $question):?>
+                            <td>
+                                <?= $question->getQuestion(); ?>
+                            </td> 
+                            <td>
+                                <a href="modifier.php?&id=$question->getId()" title="modifier"><i class="bi bi-pencil-square"></i></a>
+                            </td>
+                            <td>
+                                <a href="supprimer.php?&id=$id" title="supprimer"><i class="bi bi-x-square"></i></a>
+                            </td>
+                        <?php endforeach; ?>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </main>
     <script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
+
+
