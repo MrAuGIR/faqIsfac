a7b55475e115b12f01ed7fe0a50c2c22a4338074
handgood <handgood86@gmail.com>
2021-02-16 12:40:27 +0100

    page accueil html/css

--- index.php
+++ index.php
@@ -10,20 +10,31 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Test</h1>
-    <div class="accordion" id="accordionExample">
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="headingOne">
-            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                Accordion Item #1
-            </button>
-            </h2>
-            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                    LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA v v LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA
+    <?php require_once 'header.php';
+    ?>
+    <div class="container">
+        <div class="row">
+            <div class="col col-sm-3  col-md-2 col-lg-2">
+            <button class="btn btn-outline-success" style="width:100%;" href="#" type="submit">Telecharger</button>
+            </div>
+            <div class="col col-sm-9  col-md-10 col-lg-10">
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Accordion Item #1
+                        </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA v v LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA LOREM IPSUM BLABLA
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+        <a href="#" ><img src="img/backDebutPage.png" style="width:50px;" alt="..."></a>
     </div>
     <script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
